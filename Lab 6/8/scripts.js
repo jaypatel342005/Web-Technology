7ba6b4fbd5be2976575bc9cba859a7c445d82a0f
jaypatel342005 <pateljay97378@gmail.com>
2024-07-20 20:30:13 +0530

    Web-Technology

---
+++ Lab 6/8/scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.getElementById('heading');
+    const timeButton = document.getElementById('timeButton');
+    const redButton = document.getElementById('redButton');
+    const greenButton = document.getElementById('greenButton');
+    const timeDisplay = document.getElementById('timeDisplay');
+
+    heading.addEventListener('mouseover', function() {
+        heading.style.color = 'yellow';
+    });
+
+    heading.addEventListener('mouseout', function() {
+        heading.style.color = 'black';
+    });
+
+    timeButton.addEventListener('click', function() {
+        const currentDateTime = new Date();
+        timeDisplay.textContent = `Current Date and Time: ${currentDateTime}`;
+    });
+
+    redButton.addEventListener('click', function() {
+        document.body.style.backgroundColor = 'red';
+    });
+
+    greenButton.addEventListener('click', function() {
+        document.body.style.backgroundColor = 'green';
+    });
+});
