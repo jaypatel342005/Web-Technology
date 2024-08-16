708b243a28fb19c235611e7b986d6a27bb1e709e
jaypatel342005 <pateljay97378@gmail.com>
2024-08-16 14:11:37 +0530

    Web-Technology

--- Lab 19/Lab19_2.js
+++ Lab 19/Lab19_2.js
@@ -27,7 +27,8 @@ app.get('/portfolio', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+  });
+  
